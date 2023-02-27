47fe473454bd0c4d9697a9b224ef44e556aae42f
Julie Munn <jmunn302@gmail.com>
2023-02-27 16:40:13 +0100

    seeded user db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts 'Deleting db'
+User.destroy_all
+
+
+require "open-uri"
+
+20.times do
+  puts 'Opening unsplash'
+  file = URI.open("https://source.unsplash.com/random/300x300/?face")
+  puts file
+  puts 'Creating user'
+  user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, password: Faker::Internet.password(min_length: 8), address: Faker::Address.city, bio: Faker::Lorem.word )
+  puts user.id
+  puts 'Attaching photo'
+  user.photo.attach(io: file, filename: user.first_name, content_type: "image/jpg")
+  puts 'done!'
+end
